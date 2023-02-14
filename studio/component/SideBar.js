0e6564d568106b146c26eb375c27e516a57b8ab5
JohnMigwi <johnmigwi101@gmail.com>
2023-02-14 16:23:49 +0300

    side bar featured

---
+++ studio/component/SideBar.js
@@ -0,0 +1,16 @@
+import { MdStar } from 'react-icons/md';
+import S from '@sanity/desk-tool/structure-builder';
+
+function Sidebar() {
+  return S.list()
+    .title('Space-Hub')
+    .items([
+      S.listItem()
+        .title('Featured')
+        .id('featuredItems')
+        .icon(MdStar)
+        .child(S.editor().schemaType('featured').documentId('featuredItems')),
+    ]);
+}
+
+export default Sidebar;
