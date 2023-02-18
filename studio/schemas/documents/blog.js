3903d0a10cb00c8d9a437c71c60c41d10f185c17
JohnMigwi <johnmigwi101@gmail.com>
2023-02-18 18:51:11 +0300

    props and children

--- studio/schemas/documents/blog.js
+++ studio/schemas/documents/blog.js
@@ -41,7 +41,7 @@ export default {
       title: 'Author',
       name: 'author',
       type: 'reference',
-      to: [{ type: 'authohr' }],
+      to: [{ type: 'author' }],
     },
     {
       title: 'Cover Image',
--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -2,9 +2,9 @@ require('dotenv').config('./.env');
 
 module.exports = {
   siteMetadata: {
-    title: `TechHub-blog`,
-    siteUrl: `https://techhub-blog.netlify.com`,
-    description: `TechHub Blog is a platform for latest technology news and updates.`,
+    title: `SpaceHub-blog`,
+    siteUrl: `https://spacehub-blog.netlify.com`,
+    description: `SpaceHub Blog is a platform for latest technology news and updates.`,
   },
   plugins: [],
 };
