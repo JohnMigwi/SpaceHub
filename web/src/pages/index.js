0c6d743804b7cc03903a0f53f7e843c3f8f973d9
JohnMigwi <johnmigwi101@gmail.com>
2023-05-08 12:17:03 +0300

    Revert "changes"
    This reverts commit e6f76507c3170577b29ee57e3150413d268b0b85.

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
+<<<<<<< HEAD
 // import HeroSection from '../components/homepage/HeroSection';
+=======
+import HeroSection from '../components/homepage/HeroSection';
+>>>>>>> parent of e6f7650 (changes)
 
 const IndexPage = () => <HeroSection />;
 export default IndexPage;
