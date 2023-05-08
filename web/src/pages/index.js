93c9e81fdc0057ae6b5189aa8fa7e89102c79c4c
JohnMigwi <johnmigwi101@gmail.com>
2023-05-08 12:20:11 +0300

    reverted

--- web/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-<<<<<<< HEAD
-// import HeroSection from '../components/homepage/HeroSection';
-=======
-import HeroSection from '../components/homepage/HeroSection';
->>>>>>> parent of e6f7650 (changes)
-
-const IndexPage = () => <HeroSection />;
-export default IndexPage;
