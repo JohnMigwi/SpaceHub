fdd8c47ffabd31c30944042ba8a6fad6b7ff816b
JohnMigwi <johnmigwi101@gmail.com>
2023-05-13 21:39:36 +0300

    logo bug

--- web/src/components/logo.js
+++ web/src/components/logo.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { GrTechnology } from 'react-icons/gr';
+import { GrBug } from 'react-icons/gr';
 import LogoStyles from '../styles/LogoStyles';
 
 function Logo() {
   return (
     <LogoStyles>
-      <GrTechnology />
+      <GrBug />
     </LogoStyles>
   );
 }
