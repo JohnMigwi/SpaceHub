6ade992ba3ee562acdeeddf8696835a26c897556
JohnMigwi <johnmigwi101@gmail.com>
2023-05-13 14:56:05 +0300

    finished on heaader

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -1,33 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
-import { MdSearch } from 'react-icons/md';
+import { MdClose, MdMenu, MdSearch } from 'react-icons/md';
+import clsx from 'clsx';
 import HeaderStyles from '../styles/HeaderStyles';
 import { menu } from '../constants/menu';
 import Logo from './Logo';
+import ActionButton from './buttons/ActionButton';
 
 function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const handleNavItemClick = () => {
+    if (isNavOpen) {
+      setIsNavOpen(false);
+    }
+  };
   return (
     <HeaderStyles>
       <div className="container">
-        <div className="logo">
-          <Logo />
-        </div>
-
-        <div className="nav__wrapper">
-          <nav>
-            <ul>
-              {menu.map((item) => (
-                <li key={item.path}>
-                  <Link to={item.path}>{item.title}</Link>
-                </li>
-              ))}
-              <li className="searchIcon">
+        <div className="header__container">
+          <div className="logo">
+            <Logo />
+          </div>
+          <div className={clsx('nav__wrapper', isNavOpen && 'open')}>
+            <div className="mobileIcon">
+              <div className="searchIcon">
                 <div className="searchIcon__wrapper">
                   <MdSearch />
                 </div>
-              </li>
-            </ul>
-          </nav>
+              </div>
+              <ActionButton
+                className="mobileMenuBtn"
+                onClick={() => setIsNavOpen(true)}
+                onKeyDown={() => setIsNavOpen(true)}
+              >
+                <MdMenu />
+              </ActionButton>
+            </div>
+            {isNavOpen && (
+              <div
+                className="mobileNavBg"
+                aria-label="close menu"
+                role="button"
+                tabIndex={0}
+                onClick={() => setIsNavOpen(false)}
+                onKeyDown={() => setIsNavOpen(false)}
+              />
+            )}
+            <nav>
+              <ActionButton
+                className="mobileMenuCloseBtn"
+                onClick={() => setIsNavOpen(false)}
+                onKeyDown={() => setIsNavOpen(false)}
+              >
+                <MdClose />
+              </ActionButton>
+              <ul>
+                {menu.map((item) => (
+                  <li key={item.path}>
+                    <Link to={item.path} onClick={handleNavItemClick}>
+                      {item.title}
+                    </Link>
+                  </li>
+                ))}
+                <li className="searchIcon">
+                  <div className="searchIcon__wrapper">
+                    <MdSearch />
+                  </div>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </HeaderStyles>
