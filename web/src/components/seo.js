7351f4c366d6a4e015960f600e675e3e1ea40529
JohnMigwi <johnmigwi101@gmail.com>
2023-05-16 20:27:01 +0300

    finished on adding some SEO

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -1,10 +1,31 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-function SEO() {
+function SEO({ title, description }) {
+  const { site } = useStaticQuery(graphql`
+    query MyQuery {
+      site(siteMetadata: { description: {}, title: {} }) {
+        id
+        siteMetadata {
+          description
+          title
+        }
+      }
+    }
+  `);
+
+  const seo = {
+    title: title
+      ? `${title} - ${site.siteMetadata.title}`
+      : site.siteMetadata.title,
+    description: description || site.siteMetadata.description,
+  };
+
   return (
-    <Helmet>
-      <title>Hello World</title>
+    <Helmet title>
+      <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
     </Helmet>
   );
 }
--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import HeroSection from '../components/homepage/HeroSection';
+import SEO from '../components/seo';
 
-const IndexPage = () => <HeroSection />;
+const IndexPage = () => (
+  <>
+    <SEO />
+    <HeroSection />
+  </>
+);
 export default IndexPage;
