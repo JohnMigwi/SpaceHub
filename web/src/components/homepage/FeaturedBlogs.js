4e3d45e88b0128b6a2056c8596727c4046c05445
JohnMigwi <johnmigwi101@gmail.com>
2023-05-22 19:02:23 +0300

    unblocking

---
+++ web/src/components/homepage/FeaturedBlogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FeaturedBlogs() {
+  return (
+    <div>
+      FeaturedBlogs BETTER WORK THIS TIME HOW LONG DO YOU TAKETO LOAD MAHN
+    </div>
+  );
+}
+
+export default FeaturedBlogs;
