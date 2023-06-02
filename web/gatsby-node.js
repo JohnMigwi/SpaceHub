0e5f12535e75d1d10a81a6e5a3e87c6725bf1c18
JohnMigwi <johnmigwi101@gmail.com>
2023-06-02 17:58:26 +0300

    added more functionality on categories and fixed

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -4,9 +4,14 @@ exports.createPages = async ({ graphql, actions }) => {
   const SingleBlogTemplate = require.resolve('./src/templates/SingleBlog.js');
 
   const blogListTemplate = require.resolve('./src/templates/Blog-List.js');
-  // const categoryListTemplate = require.resolve(
-  //   './src/templates/category-list.js'
-  // );
+
+  const categoryListTemplate = require.resolve(
+    './src/templates/CategoryList.js'
+  );
+
+  const singleCategoryTemplate = require.resolve(
+    './src/templates/SingleCategory.js'
+  );
 
   const { createPage } = actions;
   const result = await graphql(`
@@ -19,15 +24,33 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
+      allSanityCategory {
+        nodes {
+          id
+          slug {
+            current
+          }
+        }
+      }
     }
   `);
 
   if (result.errors) throw result.errors;
   const blogs = result.data.allSanityBlog.nodes;
-  console.log('this is the result');
-  console.log(result);
 
+  const categories = result.data.allSanityCategory.nodes;
+
+  // a single category to display component
+
+  categories.forEach((category) => {
+    createPage({
+      path: `/categories/${category.slug.current}`,
+      component: singleCategoryTemplate,
+      context: { id: category.id },
+    });
+  });
   // Each blog page
+
   blogs.forEach((blog) => {
     createPage({
       path: `/blogs/${blog.slug.current}`,
@@ -46,6 +69,20 @@ exports.createPages = async ({ graphql, actions }) => {
         limit: postsPerPage,
         offset: index * postsPerPage,
         numberOfPages: totalBlogListPages,
+        currentPage: index + 1,
+      },
+    });
+  });
+  // category paginated pages
+  const totalCategoryListPages = Math.ceil(categories.length / postsPerPage);
+  Array.from({ length: totalCategoryListPages }).forEach((_, index) => {
+    createPage({
+      path: index === 0 ? `/categories` : `/categories/${index + 1}`,
+      component: categoryListTemplate,
+      context: {
+        limit: postsPerPage,
+        offset: index * postsPerPage,
+        numberOfPages: totalCategoryListPages,
         currentPage: index + 1,
       },
     });
