fdfe3b5d486b27ab7c41cc07ad62bcf89cbae752
JohnMigwi <johnmigwi101@gmail.com>
2023-06-08 18:01:49 +0300

    finalized all author component

--- web/src/styles/author/AuthorItemStyles.js
+++ web/src/styles/author/AuthorItemStyles.js
@@ -2,15 +2,16 @@ import { Link } from 'gatsby';
 import styled from 'styled-components';
 
 export const AuthorItemStyles = styled(Link)`
-  background-color: var(--black-2);
-  border-radius: 8px;
+  background: linear-gradient(to right, var(--black-1), transparent);
+  border-radius: 1px;
   padding: 1.5rem;
   gap: 1rem;
   display: flex;
   align-items: center;
+
   .profileImage {
-    height: 30px;
-    width: 30px;
+    height: 120px;
+    width: 120px;
     border-radius: 50%;
   }
 `;
